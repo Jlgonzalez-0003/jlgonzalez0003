8c66282b552da21858e5796953efacab01a11b13
Jlgonzalez-0003 <jlgonzalez0003@student.stcc.edu>
2024-11-13 22:42:03 -0500

    InterPlanetaryWeights.py

--- InterPlanetaryWeights.py.py
+++ InterPlanetaryWeights.py.py
@@ -1,38 +1,28 @@
 #Get the user's name and wieght.
 sName=input('What is your name? ')
-nWEIGHT=float(input('What is your weight? '))
+nWeight=float(input('What is your weight? '))
 #Get the weight of user by calculating (earth weight x planets Surface Gravity Factor).
 #Calculate weight planets.
-nMercury_SGF = 0.38
-nVenus_SGF = 0.91
-nMoon_SGF = 0.165
-nMars_SGF = 0.38
-nJupiter_SGF = 2.34
-nSaturn_SGF = 0.93
-nUranus_SGF = 0.92
-nNeptune_SGF = 1.12
-nPluto_SGF = 0.066
-
-MERCURY=float(nWEIGHT * 0.38)
-VENUS=float(nWEIGHT * 0.91)
-MOON=float(nWEIGHT * 0.165)
-MARS=float(nWEIGHT * 0.38)
-JUPITER=float(nWEIGHT * 2.34)
-SATURN=float(nWEIGHT * 0.93)
-URANUS=float(nWEIGHT * 0.92)
-NEPTUNE=float(nWEIGHT * 1.12)
-PLUTO=float(nWEIGHT * 0.066)
+nMERCURY_SGF = 0.38
+nVENUS_SGF = 0.91
+nMOON_SGF = 0.165
+nMARS_SGF = 0.38
+nJUPITER_SGF = 2.34
+nSATURN_SGF = 0.93
+nURANUS_SGF = 0.92
+nNEPTUNE_SGF = 1.12
+nPLUTO_SGF = 0.066
 
 #Display planets #Display user's name and the statement ' here are your weights on our Solar System's
 #planets:'.
 print(sName, 'here is your weights on our Soloar System\'s planets:')
 #Display the weights for the corresponding planets.
-print('Weight on MERCURY:', format(MERCURY, '10.2f'))            #Mecury weight
-print('Weight on Venus:', format(VENUS, '10.2f'))                #Venus weight
-print('Weight on Moon:', format(MOON, '10.2f'))                  #Moon weight
-print('Weight on Mars:', format(MARS, '10.2f'))                  #Mars weight
-print('Weight on Jupiter:', format(JUPITER, '10.2f'))            #Jupiter weight
-print('Weight on Saturn:', format(SATURN, '10.2f'))              #Saturn weight
-print('Weight on Uranus:', format(URANUS, '10.2f'))              #Uranus weight
-print('Weight on Neptune:', format(NEPTUNE, '10.2f'))            #Neptune weight
-print('Weight on Pluto:', format(PLUTO, '10.2f'))                #Pluto weight
+print(f'{'Weight on Mercury:':25s}{nWeight * nMERCURY_SGF:10.2f}lb')       #Mercury weight
+print(f'{'Weight on Venus:':25s}{nWeight * nVENUS_SGF:10.2f}lb')           #Venus weight
+print(f'{'Weight on Moon:':25s}{nWeight * nMOON_SGF:10.2f}lb')             #Moon weight
+print(f'{'Weight on Mars:':25s}{nWeight * nMARS_SGF:10.2f}lb')             #Mars weight
+print(f'{'Weight on Jupiter:':25s}{nWeight * nJUPITER_SGF:10.2f}lb')       #Jupiter weight
+print(f'{'Weight on Saturn:':25s}{nWeight * nSATURN_SGF:10.2f}lb')         #Saturn weight
+print(f'{'Weight on Uranus:':25s}{nWeight * nURANUS_SGF:10.2f}lb')         #Uranus weight
+print(f'{'Weight on Neptune:':25s}{nWeight * nNEPTUNE_SGF:10.2f}lb')       #Neptune weight
+print(f'{'Weight on Pluto:':25s}{nWeight * nPLUTO_SGF:10.2f}lb')           #Pluto weight
