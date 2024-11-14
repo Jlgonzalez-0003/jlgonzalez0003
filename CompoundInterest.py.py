79ce05d89b8e8aca47eb2da99aa7855f7f1da5b8
Jlgonzalez-0003 <jlgonzalez0003@student.stcc.edu>
2024-11-13 22:43:29 -0500

    CompoundInterest.py

--- CompoundInterest.py.py
+++ CompoundInterest.py.py
@@ -2,7 +2,7 @@
 nPrincipal = int(input('Enter the starting principal: '))
 
 #Get the interest rate.
-nRATE = float(input('Enter the annual interest rate: '))
+nRate = float(input('Enter the annual interest rate: '))
 
 #Get the how many times per year the interest is compounded.
 nCompound = int(input('How many times per year is the interest compounded? '))
@@ -12,7 +12,7 @@ nYears = int(input('For how many years will the account earn interest? '))
 
 #Calculate the future value by calculating the time and interest rate in which the
 #investment will be compounding.
-nFuture_value = nPrincipal *(1 + 0.025/12)**(12*2)
+nFuture_value = nPrincipal *(1 + nRate/100/nCompound)**(nCompound * nYears)
 
 #Display the future value after calculations have been made.
-print('At the end of 2 years you will have $', format(nFuture_value, '.2f'))
+print(f'{'At the end of 2 years you will have $'}{nFuture_value: ,.2f}')
